21df0a6f2eb08cd0b86fbfcaf282fb638eed5f11
princenyamadi <princenyamadi.pn@gmail.com>
2020-12-09 18:14:15 +0000

    Working with PIP

--- app/src/main/java/tech/peny/pipfun/MainActivity.java
+++ app/src/main/java/tech/peny/pipfun/MainActivity.java
@@ -2,13 +2,43 @@ package tech.peny.pipfun;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.res.Configuration;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    public void goPip(View view){
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+            enterPictureInPictureMode();
+        }
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void onPictureInPictureModeChanged(boolean isInPictureInPictureMode, Configuration newConfig) {
+        super.onPictureInPictureModeChanged(isInPictureInPictureMode, newConfig);
+
+        TextView textView = findViewById(R.id.textView);
+        Button pipButton = findViewById(R.id.pipButton);
+
+
+        if(isInPictureInPictureMode){
+//            GOING INTO PIP
+        pipButton.setVisibility(View.INVISIBLE);
+        getSupportActionBar().hide();
+        textView.setText("$10, 043.53");
+        }else{
+//            OUT OF PICTURE IN PICTURE
+            pipButton.setVisibility(View.VISIBLE);
+            getSupportActionBar().show();
+            textView.setText("Hello World");
+        }
     }
 }
